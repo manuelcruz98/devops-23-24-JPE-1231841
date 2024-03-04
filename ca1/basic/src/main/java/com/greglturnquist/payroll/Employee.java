e3abe85a2cd076ae9b11a3f57d4fa11f74e12e22
manuelcruz98 <1231841@isep.ipp.pt>
2024-03-04 17:40:53 +0000

    #3 - Add email field feature to Employee class and DatabaseLoader

--- ca1/basic/src/main/java/com/greglturnquist/payroll/Employee.java
+++ ca1/basic/src/main/java/com/greglturnquist/payroll/Employee.java
@@ -20,6 +20,8 @@ import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author Greg Turnquist
@@ -28,23 +30,24 @@ import javax.persistence.Id;
 @Entity // <1>
 public class Employee {
 
-    private @Id
-    @GeneratedValue Long id; // <2>
+    private @Id @GeneratedValue Long id; // <2>
     private String firstName;
     private String lastName;
     private String description;
     private int jobYears;
+    private String email;
 
     private Employee() {
     }
 
-    public Employee(String firstName, String lastName, String description, int jobYears) {
-        if (firstName == null || firstName.isEmpty() || lastName == null || lastName.isEmpty() || description == null || description.isEmpty() || jobYears < 0)
-            throw new IllegalArgumentException("First name, last name, and description must not be null or empty. Job years must be greater than 0.");
+    public Employee(String firstName, String lastName, String description, int jobYears, String email) {
+        if (firstName == null || firstName.isEmpty() || lastName == null || lastName.isEmpty() || description == null || description.isEmpty() || jobYears < 0 || email == null || email.isEmpty())
+            throw new IllegalArgumentException("First name, last name, description and email must not be null or empty. Job years must be greater than 0.");
         this.firstName = firstName;
         this.lastName = lastName;
         this.description = description;
         this.jobYears = jobYears;
+        this.email = email;
     }
 
     @Override
@@ -56,13 +59,14 @@ public class Employee {
                 Objects.equals(firstName, employee.firstName) &&
                 Objects.equals(lastName, employee.lastName) &&
                 Objects.equals(description, employee.description) &&
-                jobYears == employee.jobYears;
+                jobYears == employee.jobYears &&
+                Objects.equals(email, employee.email);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(id, firstName, lastName, description, jobYears);
+        return Objects.hash(id, firstName, lastName, description, jobYears, email);
     }
 
     public Long getId() {
@@ -105,6 +109,14 @@ public class Employee {
         this.jobYears = jobYears;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
@@ -113,6 +125,7 @@ public class Employee {
                 ", lastName='" + lastName + '\'' +
                 ", description='" + description + '\'' +
                 ", jobYears=" + jobYears + '\'' +
+                ", email=" + email +
                 '}';
     }
 }
